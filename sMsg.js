8367763159f895e3f67223b9033f2b343b475854
snovakovic <stefan.novakovich@gmail.com>
2015-04-09 08:52:30 +0200

    v 0.1

---
+++ sMsg.js
@@ -0,0 +1,23 @@
+
+/************************************
+ * sMsg : https://github.com/snovakovic/sMsg
+ * author: stefan.novakovich@gmail.com (www.is.gd/snovakovic)
+ * v0.1
+ ****************************************/
+var sMsg = (function () {
+	var subscribers = [];
+	return {
+		on: function ( subscribe, callback ) {
+			subscribers[subscribe] = subscribers[subscribe] ? subscribers[subscribe] : [];
+			subscribers[subscribe].push(callback);
+		},
+		getAllSubscribers: function() {
+			return subscribers;
+		},
+		broadcast: function( to, obj ) {
+			for (i = 0; i < subscribers[to].length; i++) {
+				subscribers[to][i](obj);
+			}
+		}
+	}
+})();
